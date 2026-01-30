afb9df3c576b8fded386c31f32e622b9ab824baa
JoseAlvesdev <josehenriques10@outlook.com>
2026-01-30 20:53:39 -0300

    first commit

--- backend/api/v1/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from api.v1.endpoints import email
-
-api_router =APIRouter()
-api_router.include_router(email.router, prefix='/email', tags=['email'])
-
-if __name__ == '__main__':
-    pass
