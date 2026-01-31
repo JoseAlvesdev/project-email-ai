c61f59e7912b2145b8b9e9f04a8be0029cf6dc23
JoseAlvesdev <josehenriques10@outlook.com>
2026-01-31 11:49:09 -0300

    refactor: atualizado urls de origem permitas

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,15 @@ from api.v1.api import api_router
 app = FastAPI()
 app.include_router(api_router, prefix='/api/v1')
 
+origins = [
+    "https://project-email-ai-frontend.vercel.app",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://project-email-ai-frontend.vercel.app/"],  # Em desenvolvimento substitua por "*""
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
